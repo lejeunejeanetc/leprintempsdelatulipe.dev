01f8d0144f9935db52bf0725c948bedbef2818d1
Lamjahdi Soumaya <lamjahdi.soumaya@gmail.com>
2017-05-25 15:34:32 +0200

    changement du contenu des pages d'activités et qui sommes nous

--- application/views/main/SoutienParents.php
+++ application/views/main/SoutienParents.php
@@ -1,10 +1,9 @@
 <h1>Soutien à la parentalité</h1>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae eaque beatae commodi cum. Ipsum pariatur cupiditate, odit quia, explicabo reprehenderit aliquam, quo sit optio error atque voluptate tempore animi dolore.</p>
 <div class="row">
     <div class="col-md-6">
-        <h3>Lorem</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae eaque beatae commodi cum. Ipsum pariatur cupiditate, odit quia, explicabo reprehenderit aliquam, quo sit optio error atque voluptate tempore animi dolore.</p></div>
+        <h3>Description</h3>
+        <p>Cet atelier s’inscrit en parallèle du soutien scolaire à destination des enfants. Conscients des différents acteurs gravitant autour de l’enfant et jouant un rôle primordial dans son épanouissement et son éducation, le parent est l’acteur premier et principal jouant un tel rôle. C’est pourquoi nous avons mis en place des ateliers parents afin de les outiller dans des problématiques face auxquelles ils peuvent parfois se sentir démunis.</p></div>
     <div class="col-md-6">
-        <h3>Ipsum !</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae eaque beatae commodi cum. Ipsum pariatur cupiditate, odit quia, explicabo reprehenderit aliquam, quo sit optio error atque voluptate tempore animi dolore.</p></div>
+        <h3>Atelier "Comment renforcer la confiance en soir de mon enfant"</h3>
+        <p>Notre premier atelier a permis aux parents de comprendre l’enjeu d’avoir une bonne estime de soi, comment la nourrir et la développer chez l’enfant …</p></div>
 </div>
--- application/views/session/logged.php
+++ application/views/session/logged.php
@@ -6,68 +6,75 @@ defined('BASEPATH') OR exit('No direct script access allowed');?>
             ?>
     <div class="row text-center">
         <div class="col-xs-12">
-            <h2>Interface Admin , Bienvenue
+            <h2>Interface Admin : Bienvenue
                 <?php echo  $this->session->userdata['login']; ?> ! </h2>
         </div>
     </div>
+        <div class="row text-center">
+            <h4>Gestion newsletter</h4>
+            <p><a href="<?php echo base_url('maintulipe/create_csv'); ?>">Télécharger le fichier CSV</a></p>
+        </div>
+
+        <div class="row">
+        
+                <h4 class="text-center">Gestion des événements</h4>
+            <?php
+            foreach($adminDashboard->result() as $row){       
+            ?>
+                <div class="row text-center admin-eventitems">
+                    <div class="col-xs-5 col-xs-push-3">
+                        <p>
+                            <?php echo $row->nom?>
+                        </p>
+                        <p>
+                            <?php echo $row->description?>
+                        </p>
+                    </div>
+                    <div class="col-xs-3 col-xs-push-3">
+                        <a href="<?php echo base_url(" Events/modify/$row->id")?>" class="btn btn-primary">Modifier</a>
+                        <!--<a href="<?php echo base_url(" Events/delete/$row->id")?>" class="btn btn-danger">Supprimer</a>-->
 
 
-    <?php
-    foreach($adminDashboard->result() as $row){       
-    ?>
-        <div class="row text-center admin-eventitems">
-            <div class="col-xs-5 col-xs-push-3">
-                <p>
-                    <?php echo $row->nom?>
-                </p>
-                <p>
-                    <?php echo $row->description?>
-                </p>
-            </div>
-            <div class="col-xs-3 col-xs-push-3">
-                <a href="<?php echo base_url(" Events/modify/$row->id")?>" class="btn btn-primary">Modifier</a>
-                <!--<a href="<?php echo base_url(" Events/delete/$row->id")?>" class="btn btn-danger">Supprimer</a>-->
+                        <button data-toggle="modal" data-target="#ModalDelete" class="btn btn-danger">Supprimer</button>
 
 
-                <button data-toggle="modal" data-target="#ModalDelete" class="btn btn-danger">Supprimer</button>
+                        <div id="ModalDelete" class="modal fade" role="dialog">
+                            <div class="modal-dialog">
 
+                                <!-- Modal content-->
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                        <h4 class="modal-title">Attention </h4>
+                                    </div>
+                                    <div class="modal-body">
+                                        <p>Etes-vous sûr de vouloir supprimer l'évènement ? .</p>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <a href="<?php echo base_url(" Events/delete/$row->id")?>" class="btn btn-danger">Supprimer</a>
+                                        <button type="button" class="btn btn-default" data-dismiss="modal">Annuler</button>
+                                    </div>
+                                </div>
 
-                <div id="ModalDelete" class="modal fade" role="dialog">
-                    <div class="modal-dialog">
-
-                        <!-- Modal content-->
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                <h4 class="modal-title">Attention </h4>
-                            </div>
-                            <div class="modal-body">
-                                <p>Etes-vous sûr de vouloir supprimer l'évènement ? .</p>
-                            </div>
-                            <div class="modal-footer">
-                                <a href="<?php echo base_url(" Events/delete/$row->id")?>" class="btn btn-danger">Supprimer</a>
-                                <button type="button" class="btn btn-default" data-dismiss="modal">Annuler</button>
                             </div>
                         </div>
+                    </div>
+                    <div class="col-xs-3">
 
                     </div>
                 </div>
-            </div>
-            <div class="col-xs-3">
+                <?php 
+        } ?>
+                <div class="row ">
+                    <div class="col-xs-3 col-xs-push-9">
+                        <a href="<?php echo base_url(" Events/create ")?>" class="btn btn-primary">Crée un Evenement</a>
+                    </div>
+                </div>
+                <div class="row ">
+                    <div class="col-xs-3 col-xs-push-9">
+                        <a href="<?php echo base_url(" adminlogin/logout ")?>">Se déconnecter</a>
+                    </div>
+                </div>
+                <?php }?>
 
-            </div>
         </div>
-        <?php 
-} ?>
-        <div class="row ">
-            <div class="col-xs-3 col-xs-push-9">
-                <a href="<?php echo base_url(" Events/create ")?>" class="btn btn-primary">Crée un Evenement</a>
-            </div>
-        </div>
-        <div class="row ">
-            <div class="col-xs-3 col-xs-push-9">
-                <a href="<?php echo base_url(" adminlogin/logout ")?>">Logout</a>
-            </div>
-        </div>
-        <?php }?>
-
